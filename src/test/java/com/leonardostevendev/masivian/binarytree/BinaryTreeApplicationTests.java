6cfce82d3381e2031ae5a1d21f337fedefc249c0
Steven <leo.diaz.96@hotmail.com>
2019-08-20 18:41:33 -0500

    Yajuuuuuu !! los tests funcionan... ahora solo falta programar el endpoint faltante.

--- src/test/java/com/leonardostevendev/masivian/binarytree/BinaryTreeApplicationTests.java
+++ src/test/java/com/leonardostevendev/masivian/binarytree/BinaryTreeApplicationTests.java
@@ -1,7 +1,9 @@
 package com.leonardostevendev.masivian.binarytree;
 
+import com.leonardostevendev.masivian.binarytree.structure.Node;
 import com.leonardostevendev.masivian.binarytree.structure.Tree;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,17 +15,29 @@ import static junit.framework.TestCase.assertEquals;
 @SpringBootTest
 public class BinaryTreeApplicationTests {
 
-    Tree tree;
+    static Tree tree;
 
-    @Before
-    public void prepareTree() {
+    @BeforeClass
+    public static void prepareTree() {
+        tree = new Tree(67);
+        tree.addNode(new Node(39));
+        tree.addNode(new Node(76));
+        tree.addNode(new Node(28));
+        tree.addNode(new Node(44));
+        tree.addNode(new Node(74));
+        tree.addNode(new Node(85));
+        tree.addNode(new Node(29));
+        tree.addNode(new Node(83));
+        tree.addNode(new Node(87));
 
     }
 
     @Test
     public void testLCA() {
-        Tree tree = new Tree(39);
-        assertEquals(0, 0);
+        System.out.println(tree.getRoot());
+        assertEquals(39, tree.lowestCommonAncestor(tree.getRoot(), tree.searchNode(tree.getRoot(), 29), tree.searchNode(tree.getRoot(), 44)).getVal());
+        assertEquals(67, tree.lowestCommonAncestor(tree.getRoot(), tree.searchNode(tree.getRoot(), 44), tree.searchNode(tree.getRoot(), 85)).getVal());
+        assertEquals(85, tree.lowestCommonAncestor(tree.getRoot(), tree.searchNode(tree.getRoot(), 83), tree.searchNode(tree.getRoot(), 87)).getVal());
     }
 
 }
